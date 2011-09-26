a742ea6954dae8049ac525f297b6c9fa1e7a99c4
rgalanakis <rgalanakis@rgalanakis-dm1z.hsd1.ga.comcast.net.>
2011-09-26 19:06:15 -0400

    Added _pynoclecover module so pynocle wouldn't be imported in order to use the run_with_coverage function when testing.
    Added unit tests.  Up to 42% coverage! :P

---
+++ _pynoclecover.py
@@ -0,0 +1,12 @@
+"""Small module that contains the 'run_with_coverage' method so that pynocle test coverage doesn't import it before
+it starts coverage.
+"""
+import coverage
+
+def run_with_coverage(func, **coveragekwargs):
+    cov = coverage.coverage(**coveragekwargs)
+    cov.start()
+    result = func()
+    cov.stop()
+    cov.save()
+    return result, cov
--- test_init.py
+++ test_init.py
@@ -1,14 +1,15 @@
 import nose
-import pynocle
 
 if __name__ == '__main__':
+    import _pynoclecover
     try:
-        result, cov = pynocle.run_with_coverage(nose.run)
+        result, cov = _pynoclecover.run_with_coverage(nose.run)
     except TypeError as exc:
         #Under debugger in pycharm, it uses the wrong coverage module!
         if exc.args[0] == "__init__() got an unexpected keyword argument 'data_file'":
             cov = None
         else:
             raise
+    import pynocle
     m = pynocle.Monocle(outputdir='exampleoutput', coveragedata=cov)
     m.generate_all()
