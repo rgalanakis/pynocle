c677631f67178ae97a2979c90919f0ba0539e487
rgalanakis <rgalanakis@rgalanakis-dm1z.hsd1.ga.comcast.net>
2011-09-30 19:22:51 -0400

    Preliminary funcinfo support.  Going to rework the import module, then go back to supporting it.

---
+++ funcinfo/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import compiler
+
+import pynocle.utils as utils
+
+class FuncInfo(object):
+    def __init__(self, filename, astnode):
+        self.filename = filename
+
+def all_func_nodes(astnode):
+        isclass = lambda x: isinstance(x, compiler.ast.Class)
+        nodes = filter(isclass, utils.flatten(astnode, lambda x: x.getChildNodes()))
+        return nodes
+
+def extract_funcinfos(*filenames):
+    result = []
+    for f in filenames:
+        try:
+            ast = compiler.parseFile(f)
+        except SyntaxError:
+            continue
+        for node in all_func_nodes(ast):
+            fi = FuncInfo(f, node)
+            result.append(fi)
+    return result
