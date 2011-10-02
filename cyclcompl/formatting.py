646de1c9c875ed0fb78de60f2813f2d94445cdb4
rgalanakis <rgalanakis@rgalanakis-dm1z.hsd1.ga.comcast.net>
2011-10-02 18:59:34 -0400

    Big formatting optimization and cleanup.  Got rid of a bunch of duplicated code. Added more documentation. Added generation of ALL supported types to test_init.py

--- cyclcompl/formatting.py
+++ cyclcompl/formatting.py
@@ -9,29 +9,43 @@ import pynocle.utils as utils
 DEFAULT_THRESHOLD = 6
 
 class _CCFormatter(utils.IReportFormatter):
-    def __init__(self, out=sys.stdout, threshold=None, writeempty=False, leading_path=None):
+    """Base class for formatting Cyclomatic Complexity reports.
+
+    out: The stream to write the report out to.
+    threshold: Items with CC values less than threshold will not be included in the report.
+    leading_path: Strip off this leading path from result filenames.
+    """
+    def __init__(self, out=sys.stdout, threshold=None, leading_path=None):
         if threshold is None:
             threshold = DEFAULT_THRESHOLD
         elif threshold < 1:
             raise ValueError, 'threshold must be greater than 0, got ' + str(threshold)
-        self.writeempty = writeempty
         self.threshold = threshold
         self._outstream = out
         self.leading_path = leading_path
 
     def above_threshold(self, flatstat):
+        """Returns true of the CC of flatstat is equal to or above self.threshold."""
         return flatstat[2] >= self.threshold #ind 2 is cc amount
 
     def prettify(self, s):
+        """Calls utils.prettify on s with self.leadingpath."""
         return utils.prettify_path(s, self.leading_path)
 
-class CCTextFormatter(_CCFormatter):
+    def infostr(self, linebreak='', newline='<br />'):
+        """Returns the informational string about CC."""
+        return ('Cyclomatic Complexity is a measure of decisions that can be made in a procedure.{1}'
+            'Values <= 10 are fine, between 11 and 20 should be refactored, and values above 20 are usually{0}'
+            'considered unacceptable and should be refactored.').format(linebreak, newline)
 
+    def showingstr(self):
+        return 'Showing items with a CC greater than or equal to %s.' % self.threshold
+
+class CCTextFormatter(_CCFormatter):
     def format_report_header(self):
-        self._outstream.write('Cyclomatic Complexity is a measure of decisions that can be made in a procedure.\n'
-            'Values <= 10 are fine, between 11 and 20 should be refactored, and values above 20 are usually\n'
-            'considered unacceptable and should be refactored.\n\n')
-        self._outstream.write('Showing items with a CC greater than or equal to %s\n\n' % self.threshold)
+        self.outstream().write(self.infostr('\n', '\n'))
+        self.outstream().write('\n\n')
+        self.outstream().write('%s\n\n' % self.showingstr())
 
     def format_failures(self, failures):
         if not failures:
@@ -69,19 +83,21 @@ class CCTextFormatter(_CCFormatter):
             self._outstream.write(filename)
             self._outstream.write('\n')
             tbl.write(self.outstream())
-        elif self.writeempty:
-            self._outstream.write('No items with a CC >= %s\n\n' % self.threshold)
         return tbl.rows
 
 
 class CCGoogleChartFormatter(_CCFormatter):
+    def __init__(self, *args, **kwargs):
+        super(CCGoogleChartFormatter, self).__init__(*args, **kwargs)
+        self.chart = tableprint.GoogleChartTable(
+            [('Filename', 'string'), ('Type', 'string'), ('Name', 'string'), ('CC', 'number')])
 
     def format_report_header(self):
-        cols = ('Filename', 'string'), ('Type', 'string'), ('Name', 'string'), ('CC', 'number')
-        self.outstream().write(tableprint.googlechart_table_html_header(colnames_and_types=cols))
+        self.outstream().write(self.chart.first_part())
 
     def format_report_footer(self):
-        self.outstream().write(tableprint.googlechart_table_html_footer())
+        firstp = '<p>%s</p>\n<p>%s</p>' % (self.infostr(), self.showingstr())
+        self.outstream().write(self.chart.last_part(abovetable=firstp))
 
     def format_data(self, files_stats_failures):
         """Formats the output of measure_cyclcompl ([filename, stats], [failures])."""
@@ -90,8 +106,6 @@ class CCGoogleChartFormatter(_CCFormatter):
             filename = utils.prettify_path(filename, self.leading_path)
             for type, name, cc in filter(self.above_threshold, stats.flatStats):
                 rows.append([filename, type, name, cc])
-        for row in rows:
-            self.outstream().write('        data.addRow(%s);\n' % row)
-
+        self.outstream().write(self.chart.second_part(rows))
 
 formatter_registry = utils.ExtensionFormatterRegistry({'.txt': CCTextFormatter, '.html': CCGoogleChartFormatter})
--- sloc/formatting.py
+++ sloc/formatting.py
@@ -6,6 +6,11 @@ import pynocle.tableprint as tableprint
 import pynocle.utils as utils
 
 class _SlocFormatter(utils.IReportFormatter):
+    """Base class for formatters for SLOC info.
+
+    out: The stream to write the report to.
+    leading_path: The path to strip off from filenames in the report.
+    """
     def __init__(self, out=sys.stdout, leading_path=None):
         self.out = out
         self.leading_path = leading_path
@@ -14,52 +19,67 @@ class _SlocFormatter(utils.IReportFormatter):
         return self.out
 
     def _fmtperc(self, i):
-        """Format number i as a percentage."""
+        """Format number i as a percentage, to one decimal place."""
         perc = '%.1f%%' % (i * 100)
         return perc
 
-class SlocTextFormatter(_SlocFormatter):
-    """Functionality for formatting SLOC info into a readable file."""
-    def format_report_header(self):
-        """Prints out a SLOC explanation and header to self.out."""
-        self.out.write('SLOC (Source Lines of Code)\n')
-        self.out.write('Measures physical lines of source code (SLOC), lines of comments, and blank lines, in\n'
-            'number and percentage of file.  Also measures total line count (TLOC) and as percentage of\n'
-            'total codebase lines.\n\n')
+    def infostr(self, linebreak='', newline='<br />'):
+        return ('Measures physical source lines of code (SLOC), lines of comments, and blank lines, in{0}'
+            'number and percentage of file.{1}'
+            'Also measures total line count and as percentage of total codebase lines.').format(linebreak, newline)
 
     def _get_totals_row(self, slocgroup):
-        tl = slocgroup.totallines
+        """Returns the row for TOTALS."""
+        totallines = slocgroup.totallines
         def average(key):
-            total = tl(key)
-            return self._fmtperc(total / len(slocgroup.filenamesToSlocInfos))
-        return map(str, ['TOTALS',
-                         tl('code'), average('codeperc'),
-                         tl('comment'), average('commentperc'),
-                         tl('blank'), average('blankperc'),
-                         tl('total'), self._fmtperc(tl('totalperc'))])
+            total = totallines(key)
+            return total / len(slocgroup.filenamesToSlocInfos)
+        return ['TOTALS',
+                 totallines('code'), average('codeperc'),
+                 totallines('comment'), average('commentperc'),
+                 totallines('blank'), average('blankperc'),
+                 totallines('total'), totallines('totalperc')]
+
+    def create_rows(self, slocgroup):
+        """Returns a list of rows.  The caller may need to call _stringify on them for display."""
+        rows = []
+        sortedbyfilename = sorted(slocgroup.filenamesToSlocInfos.items(), key=lambda kvp: kvp[0])
+        for filename, d in sortedbyfilename:
+            row = [utils.prettify_path(filename, self.leading_path),
+                   d['code'], d['codeperc'],
+                   d['comment'], d['commentperc'],
+                   d['blank'], d['blankperc'],
+                   d['total'], d['totalperc']]
+            rows.append(row)
+        rows.append(self._get_totals_row(slocgroup))
+        return rows
+
+
+class SlocTextFormatter(_SlocFormatter):
+    def format_report_header(self):
+        self.out.write(self.infostr('\n', '\n'))
+        self.out.write('\n\n')
+
+    def _stringify(self, row):
+        """Returns a row/list as a list of properly formatted strings."""
+        return [row[0],
+               str(row[1]), self._fmtperc(row[2]),
+               str(row[3]), self._fmtperc(row[4]),
+               str(row[5]), self._fmtperc(row[6]),
+               str(row[7]), self._fmtperc(row[8])]
 
     def format_data(self, slocgroup):
         header = 'Filename', 'Code', 'Code%', 'Comment', 'Comment%', 'Blank', 'Blank%', 'Total', 'Total%'
         c = tableprint.JUST_C
         justs = tableprint.JUST_L, c, c, c, c, c, c, c, c
-        rows = []
-        sortedbyfilename = sorted(slocgroup.filenamesToSlocInfos.items(), key=lambda kvp: kvp[0])
-        for filename, d in sortedbyfilename:
-            row = (utils.prettify_path(filename, self.leading_path),
-                   str(d['code']), self._fmtperc(d['codeperc']),
-                   str(d['comment']), self._fmtperc(d['commentperc']),
-                   str(d['blank']), self._fmtperc(d['blankperc']),
-                   str(d['total']), self._fmtperc(d['totalperc']))
-            rows.append(row)
-        rows.append([''] * len(header)) #blank row
-        rows.append(self._get_totals_row(slocgroup))
+        rows = map(self._stringify, self.create_rows(slocgroup))
         tbl = tableprint.Table(header, rows, just=justs)
         tbl.write(self.out)
 
 
 class SlocGoogleChartFormatter(_SlocFormatter):
-
-    def format_report_header(self):
+    def __init__(self, *args, **kwargs):
+        super(SlocGoogleChartFormatter, self).__init__(*args, **kwargs)
         cols = [('Filename', 'string'),
                 ('Code', 'number'),
                 ('Code%', 'number'),
@@ -69,41 +89,32 @@ class SlocGoogleChartFormatter(_SlocFormatter):
                 ('Blank%', 'number'),
                 ('Total', 'number'),
                 ('Total%', 'number')]
-        self.outstream().write(tableprint.googlechart_table_html_header(colnames_and_types=cols))
+        self.chart = tableprint.GoogleChartTable(cols)
+
+    def format_report_header(self):
+        self.outstream().write(self.chart.first_part())
 
     def format_report_footer(self):
-        self.outstream().write(tableprint.googlechart_table_html_footer())
+        abovepart = '<p>%s</p>' % self.infostr()
+        self.outstream().write(self.chart.last_part(abovetable=abovepart))
 
     def _js_perc(self, value):
         """Return a dict for JS for formatting value as a percent."""
         s = self._fmtperc(value)
         return {'v': value, 'f': s}
-
         #{v: new Date(1999,0,1), f: 'January First, Nineteen ninety-nine'}
-    def _get_totals_row(self, slocgroup):
-        tl = slocgroup.totallines
-        def average(key):
-            total = tl(key)
-            return self._js_perc(total / len(slocgroup.filenamesToSlocInfos))
-        return ['TOTALS',
-                 tl('code'), average('codeperc'),
-                 tl('comment'), average('commentperc'),
-                 tl('blank'), average('blankperc'),
-                 tl('total'), self._js_perc(tl('totalperc'))]
+
+    def _stringify(self, row):
+        """Returns a row/list as a list of properly formatted strings."""
+        return [row[0],
+               str(row[1]), self._js_perc(row[2]),
+               str(row[3]), self._js_perc(row[4]),
+               str(row[5]), self._js_perc(row[6]),
+               str(row[7]), self._js_perc(row[8])]
 
     def format_data(self, slocgroup):
-        rows = []
-        sortedbyfilename = sorted(slocgroup.filenamesToSlocInfos.items(), key=lambda kvp: kvp[0])
-        for filename, d in sortedbyfilename:
-            row = [utils.prettify_path(filename, self.leading_path),
-                   d['code'], self._js_perc(d['codeperc']),
-                   d['comment'], self._js_perc(d['commentperc']),
-                   d['blank'], self._js_perc(d['blankperc']),
-                   d['total'], self._js_perc(d['totalperc'])]
-            rows.append(row)
-        rows.append(self._get_totals_row(slocgroup))
-        for row in rows:
-            self.outstream().write('        data.addRow(%s);\n' % row)
+        rows = map(self._stringify, self.create_rows(slocgroup))
+        self.outstream().write(self.chart.second_part(rows))
 
 
 formatter_registry = utils.ExtensionFormatterRegistry({'.txt': SlocTextFormatter, '.html': SlocGoogleChartFormatter})
