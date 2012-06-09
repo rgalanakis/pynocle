98626f963ee1c929a2e80aa6fccc8d861cb960aa
Rob Galanakis <rob.galanakis@gmail.com>
2012-06-09 16:53:31 +0000

    Getting rid of files that shouldn't be there anyway...

--- pynocle/depgraph/start_xmlrpcserver.bat
+++
@@ -1,5 +0,0 @@
-REM Demo XML RPC service launcher.  You should create your own.
-
-SET curdir=%~dp0
-python xmlrpcrenderer.py --host localhost --port 8745 --log %curdir%xmlrpcrenderer.log --dot "C:\Program Files (x86)\Graphviz 2.28\bin\dot.exe"
-pause
