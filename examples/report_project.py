f8c83b15d4c59b1f2fdacd10758d91551927b520
Rob Galanakis <rob.galanakis@gmail.com>
2012-06-10 22:37:48 +0000

    Split up into separate pynocle reporter.

--- examples/report_project.py
+++ examples/report_project.py
@@ -1,43 +1,61 @@
 #!/usr/bin/env python
+"""This script can be imported to help generate a pynocle report
+for a project.
+
+You will need coverage installed to use it,
+and pynocle should be in your sys path so it can be imported.
+"""
 
 import os
+import tempfile
 
 os.environ['path'] += os.pathsep + r'C:\Program Files (x86)\Graphviz 2.28\bin'
 
-thisdir = os.path.dirname(__file__)
-outdir = os.path.join(thisdir, 'exampleoutput')
-pynocledir = os.path.join(thisdir, '..', 'pynocle')
 
-def _get_coverage():
+def _get_coverage(projectname, docoverwork, regencover):
     import coverage
-    try:
-        cov = coverage.coverage()
-    except Exception as exc:
-        msg = exc.args[0]
-        #Under debugger in pycharm, it uses the wrong coverage module!
-        if msg == "__init__() got an unexpected keyword argument 'data_file'":
-            return None
-        if msg == "global name 'cache_location' is not defined":
-            return None
-        raise
+    covfile = os.path.join(tempfile.gettempdir(), projectname + '.coverage')
+    cov = coverage.coverage(data_file=covfile)
+
+    if not regencover and os.path.exists(covfile):
+        cov.load()
+        return cov
+
     cov.start()
-    oldcwd = os.getcwd()
-    try:
-        os.chdir(pynocledir)
-        import pynocle
-    finally:
-        os.chdir(oldcwd)
+    docoverwork()
     cov.stop()
+    cov.save()
     return cov
 
 
-def run_on_pynocle():
-    cov = _get_coverage()
+def nose_dir(cwd):
+    import nose
+    old = os.getcwd()
+    try:
+        os.chdir(cwd)
+        nose.run()
+    finally:
+        os.chdir(old)
 
+
+def run_project(projectname, rootdir, outdir,
+                docoverwork=None, regencover=True):
+    """Generates a pynocle report for a project.
+
+    :param projectname: The name of the project.
+    :param rootdir: The root directory of the files to analyze.
+    :param outdir: The folder to place report files.
+    :param docoverwork: Parameterless callable invoked under coverage
+      that the coverage report will be generated from.
+      If None, do not generate coverage.
+    :param regencover: By default, this script will cache a .coverage
+      file for the project in the temp folder. If `regencover`,
+      this cache will be regenerated.
+    """
+    cov = None
+    if docoverwork:
+        cov = _get_coverage(projectname, docoverwork, regencover)
     import pynocle
-    m = pynocle.Monocle('Pynocle', outdir, rootdir=pynocledir, coveragedata=cov)
+    m = pynocle.Monocle(
+        projectname, outdir, rootdir=rootdir, coveragedata=cov)
     m.generate_all()
-
-
-if __name__ == '__main__':
-    run_on_pynocle()
